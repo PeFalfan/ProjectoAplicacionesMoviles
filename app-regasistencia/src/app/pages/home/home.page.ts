f0e35d26ba5da44b4e0c06640e0e0badd57ef80a
Pedro Falfan <pe.falfan@duocuc.cl>
2021-09-11 23:33:08 -0300

    Construccion del sitio, y validaciones de formulario

--- app-regasistencia/src/app/pages/home/home.page.ts
+++ app-regasistencia/src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  usuarioIngresado: string;
+
+  constructor(public ruta : ActivatedRoute) { 
+    this.ruta.queryParams.subscribe(params => {
+      this.usuarioIngresado = params.usuario;
+    })
+   }
 
   ngOnInit() {
   }
--- app-regasistencia/src/app/pages/recover/recover.page.ts
+++ app-regasistencia/src/app/pages/recover/recover.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recover',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecoverPage implements OnInit {
 
-  constructor() { }
+  //usuario Original
+  usuarioOriginal: string;
+
+  //Credenciales nuevas
+  usuarioConf: string;
+  pass: string = "";
+  passConf: string = "";
+
+  constructor(public navController: NavController,
+    public rutaUsuario: ActivatedRoute) {
+    this.rutaUsuario.queryParams.subscribe(params => {
+      this.usuarioOriginal = params.usuario;
+    })
+  }
 
   ngOnInit() {
+  }
+
+  cambiarContrasena() {
+    if (this.usuarioOriginal === this.usuarioConf) {
+
+      if (this.pass === "") {
+        alert("La contraseña no puede ser vacía");
+      } else {
+
+        if (this.pass === this.passConf) {
+
+          alert("Contraseña Actualizada...");
+          this.actualizarContraseña();
+
+        } else {
+          alert("Contraseñas diferentes, favor validar.")
+        }
+      }
+
+    } else {
+      alert("Usuario no válido.")
+    }
+  }
+
+  actualizarContraseña() {
+    this.navController.navigateRoot('login',
+      {
+        queryParams: {
+          'nuevaPass': this.pass
+        }
+      });
+
   }
 
 }
