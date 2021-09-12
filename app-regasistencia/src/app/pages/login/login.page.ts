2280856e648bd77efbe87382f8ce4153fb7fac2f
Pedro Falfan <pe.falfan@duocuc.cl>
2021-09-12 01:54:34 -0300

    Se corrige logica de cambio de contraseña

--- app-regasistencia/src/app/pages/login/login.page.ts
+++ app-regasistencia/src/app/pages/login/login.page.ts
@@ -9,20 +9,23 @@ import { NavController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
-  constructor(public navController: NavController,
-    public rutita: ActivatedRoute) {
-    this.rutita.queryParams.subscribe(params => {
-
-      console.log(params.nuevaPass);
-      this.passRegistrada = params.nuevaPass;
-
-    })
-  }
-
   usuarioRegistrado: string = "Tanjirou";
   passRegistrada: string = "asd";
   usuarioIngresado: string;
   contrasena: string;
+  actualizando: boolean = false;
+
+  constructor(public navController: NavController,
+    public rutita: ActivatedRoute) {
+    this.rutita.queryParams.subscribe(params => {
+
+      if (params.nuevaPass === undefined) {
+        // ¯\_(ツ)_/¯ - (งツ)ว - ʕ •́؈•̀ ₎ - (⊃｡•́‿•̀｡)⊃ - ƪ(ړײ)‎ƪ​​
+      } else {
+        this.passRegistrada = params.nuevaPass;
+      }
+    })
+  }
 
   ngOnInit() {
   }
--- app-regasistencia/src/app/pages/recover/recover.page.ts
+++ app-regasistencia/src/app/pages/recover/recover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { LoginPage } from '../login/login.page';
 
 @Component({
   selector: 'app-recover',
